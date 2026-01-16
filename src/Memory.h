867820c1ac45f52fa8d59e2a941f667b86ccd998
Ardeniscool1293 <arden.stanley@yahoo.com>
2026-01-15 23:07:51 -0600

    Working on font loading

--- src/Memory.h
+++ src/Memory.h
@@ -8,14 +8,13 @@
 #define MEMORY_CHAR_START 0x050
 #define MEMORY_CHAR_END 0x0A0
 #define MEMORY_MAIN_START 0x200
-#define MEMROY_MAIN_END 0xFFF
+#define MEMORY_MAIN_END 0xFFF
 
 
 
 class Memory {
 private:   
 	uint8_t m_data[MEMORY_SIZE];
-	
 public:
 	Memory();
 	~Memory();
